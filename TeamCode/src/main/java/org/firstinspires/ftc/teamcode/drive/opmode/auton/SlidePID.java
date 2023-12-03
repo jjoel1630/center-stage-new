ff6a24fe110e55be5ad159c13c4307d252eee572
Joel Johnson <jjoel1630@gmail.com>
2023-12-02 22:46:41 -0800

    fixed pid tuning

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/opmode/auton/SlidePID.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/opmode/auton/SlidePID.java
@@ -10,38 +10,27 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
-import org.firstinspires.ftc.teamcode.drive.opmode.PIDController;
+import org.firstinspires.ftc.teamcode.drive.opmode.auton.PIDControllerCustom;
 
 @Config
 @Autonomous
 public class SlidePID extends LinearOpMode {
-    public PIDController controller;
     public ElapsedTime timer = new ElapsedTime();
 
-    public static double pl = 0.0147, il = 0, dl = 0.00023, fl = 0;
-    public static double pr = 0.000789, ir = 0, dr = 0.00023, fr = 0;
+    public static double pl = 0.08, il = 0, dl = 0.0008, fl = 0;
+//    public static double pr = 0.000789, ir = 0, dr = 0.00023, fr = 0;
+    public PIDControllerCustom pidController = new PIDControllerCustom(pl,il,dl);
 
     public double lastError = 0;
     public double integralSum = 0;
 
     public static double target = 500;
-    public static double powerMultiplierLeft = -1;
+    public static double powerMultiplierLeft = 1;
     public static double powerMultiplierRight = 1;
     public static long timeout = 3000;
+    public static double thresholdPower = 0.1;
 
     public DcMotorEx slideLeft, slideRight;
-
-    public double calcPowerRight(double ref, double state) {
-        double err = ref - state;
-        integralSum += err * timer.seconds();
-        double derivative = (err - lastError) / timer.seconds();
-        lastError = err;
-
-        timer.reset();
-
-        double output = (err * pr) + (derivative * dr) + (integralSum * ir) + (ref * fr);
-        return output;
-    }
 
     public double calcPowerLeft(double ref, double state) {
         double err = ref - state;
@@ -60,6 +49,7 @@ public class SlidePID extends LinearOpMode {
         slideLeft = hardwareMap.get(DcMotorEx.class, "linearSlideLeft");
         slideLeft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         slideLeft.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        slideLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         slideRight = hardwareMap.get(DcMotorEx.class, "linearSlideRight");
         slideRight.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         slideRight.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
@@ -68,20 +58,24 @@ public class SlidePID extends LinearOpMode {
 
         waitForStart();
 
+        double mx = 0;
+
         while(opModeIsActive()) {
             int curPosLeft = slideLeft.getCurrentPosition();
-            int curPosRight = slideRight.getCurrentPosition();
-            double pidLeft = calcPowerLeft(curPosLeft, target);
-            double pidRight = calcPowerRight(curPosRight, target);
+            double pidLeft = pidController.update(target, curPosLeft);
+            pidLeft = Math.abs(pidLeft) <= thresholdPower ? 0 : pidLeft;
+//            double pidLeft = calcPowerLeft(curPosLeft, target);
+//            int curPosRight = slideRight.getCurrentPosition();
+//            double pidRight = calcPowerRight(curPosRight, target);
 
             slideLeft.setPower(powerMultiplierLeft * pidLeft);
-            slideRight.setPower(powerMultiplierRight * pidRight);
+
+            mx = Math.max(mx, Math.abs(powerMultiplierLeft * pidLeft));
 
             telemetry.addData("curPosLeft", curPosLeft);
-            telemetry.addData("curPosRight", curPosRight);
             telemetry.addData("power left", pidLeft);
-            telemetry.addData("power right", pidLeft);
             telemetry.addData("targetPos", target);
+            telemetry.addData("max power", mx);
             telemetry.update();
         }
     }
