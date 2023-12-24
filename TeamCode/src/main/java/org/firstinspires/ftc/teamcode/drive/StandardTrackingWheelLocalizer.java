ecb40c85c70819e09df9ce39b3404e2396236424
Nikita Naik <nikita.sachin.naik@gmail.com>
2023-12-23 22:33:31 -0800

    added tuning values

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/StandardTrackingWheelLocalizer.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/StandardTrackingWheelLocalizer.java
@@ -31,10 +31,10 @@ public class StandardTrackingWheelLocalizer extends ThreeTrackingWheelLocalizer 
     public static double WHEEL_RADIUS = 0.6889764; // in
     public static double GEAR_RATIO = 1; // output (wheel) speed / input (encoder) speed
 
-    public static double LATERAL_DISTANCE = 14.625; // in; distance between the left and right wheels
+    public static double LATERAL_DISTANCE = 14.25; // in; distance between the left and right wheels
     public static double FORWARD_OFFSET = 2.625; // in; offset of the lateral wheel
 
-    public static double X_MULTIPLIER = 1.012475; // Multiplier in the X direction
+    public static double X_MULTIPLIER = 1; // Multiplier in the X direction
     public static double Y_MULTIPLIER = 1; // Multiplier in the Y direction
 
     private Encoder leftEncoder, rightEncoder, frontEncoder;
