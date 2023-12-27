463e3f31b23c684de009ecbb231b5d8b3cdb8d1c
Joel Johnson <jjoel1630@gmail.com>
2023-12-27 10:58:19 -0800

    started linear slide subsystem

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/opmode/subsystems/LinearSlide.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/opmode/subsystems/LinearSlide.java
@@ -1,4 +1,7 @@
 package org.firstinspires.ftc.teamcode.drive.opmode.subsystems;
 
 public class LinearSlide {
+    public double Kp, Ki, Kd, Kf;
+    public double currentPos;
+    public double targetPos;
 }
