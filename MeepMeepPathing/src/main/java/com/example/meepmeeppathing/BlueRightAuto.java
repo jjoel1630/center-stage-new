11125691e52c7db5c37af9d083663575098664a5
Nikita Naik <nikita.sachin.naik@gmail.com>
2024-01-02 16:01:00 -0800

    redright and blueright paths

--- MeepMeepPathing/src/main/java/com/example/meepmeeppathing/BlueRightAuto.java
+++ MeepMeepPathing/src/main/java/com/example/meepmeeppathing/BlueRightAuto.java
@@ -18,35 +18,31 @@ public class BlueRightAuto {
                 // Option: Set theme. Default = ColorSchemeRedDark()
                 .followTrajectorySequence(drive ->
                         // place pixel 1
-                                drive.trajectorySequenceBuilder(new Pose2d(16.29, 66.25, Math.toRadians(-90.00)))
-                                        .splineTo(new Vector2d(14.81, 46.94), Math.toRadians(265.63))
-                                        .splineTo(new Vector2d(0.52, 33.68), Math.toRadians(-85.83))
-                                        .splineTo(new Vector2d(2.43, 32.94), Math.toRadians(-21.04))
-                                        .splineTo(new Vector2d(48.42, 36.92), Math.toRadians(-1.86))
-                                        .splineTo(new Vector2d(49.74, 43.11), Math.toRadians(77.59))
-                                        .splineTo(new Vector2d(48.71, 53.13), Math.toRadians(92.49))
-                                        .splineTo(new Vector2d(61.39, 60.06), Math.toRadians(13.09))
+                            /*    drive.trajectorySequenceBuilder(new Pose2d(14.87, 62.65, Math.toRadians(270.00)))
+                                        .lineToLinearHeading(new Pose2d(10.83, 36.11, Math.toRadians(270.00)))
+                                        .lineToLinearHeading(new Pose2d(1.06, 35.68, Math.toRadians(270.00)))
+                                        .lineToSplineHeading(new Pose2d(49.06, 36.11, Math.toRadians(0.00)))
+                                        .lineToLinearHeading(new Pose2d(49.27, 60.53, Math.toRadians(0.00)))
+                                        .lineToLinearHeading(new Pose2d(55.43, 60.32, Math.toRadians(0.00)))
                                         .build()
-                       /* // place pixel 2
-                       drive.trajectorySequenceBuilder(new Pose2d(12.45, 62.86, Math.toRadians(-90.00)))
-                        .splineTo(new Vector2d(5.53, 32.20), Math.toRadians(257.27))
-                        .splineTo(new Vector2d(9.06, 26.16), Math.toRadians(-23.70))
-                        .splineTo(new Vector2d(51.22, 30.73), Math.toRadians(4.56))
-                        .splineTo(new Vector2d(49.74, 33.09), Math.toRadians(122.01))
-                        .splineTo(new Vector2d(46.35, 42.37), Math.toRadians(110.06))
-                        .splineTo(new Vector2d(51.07, 56.97), Math.toRadians(72.09))
-                         .splineTo(new Vector2d(63.30, 59.03), Math.toRadians(-8.97))
-                        .build()
+
+                       // place pixel 2
+                                drive.trajectorySequenceBuilder(new Pose2d(14.87, 62.65, Math.toRadians(270.00)))
+                                        .lineToLinearHeading(new Pose2d(16.78, 24.64, Math.toRadians(270.00)))
+                                        .lineToSplineHeading(new Pose2d(49.06, 36.11, Math.toRadians(0.00)))
+                                        .lineToLinearHeading(new Pose2d(49.27, 60.53, Math.toRadians(0.00)))
+                                        .lineToLinearHeading(new Pose2d(55.43, 60.32, Math.toRadians(0.00)))
+                                        .build()
                         */
 
-/*                        // place pixel 3
-                        drive.trajectorySequenceBuilder(new Pose2d(8.03, 65.07, Math.toRadians(-90.00)))
-                        .splineTo(new Vector2d(22.62, 36.63), Math.toRadians(-88.60))
-                        .splineTo(new Vector2d(50.78, 35.74), Math.toRadians(0.62))
-                        .splineTo(new Vector2d(50.63, 39.57), Math.toRadians(92.20))
-                        .splineTo(new Vector2d(48.27, 53.43), Math.toRadians(104.22))
-                        .splineTo(new Vector2d(62.57, 59.32), Math.toRadians(19.93))
-                         .build()*/
+
+                       // place pixel 3
+                        drive.trajectorySequenceBuilder(new Pose2d(14.87, 62.65, Math.toRadians(270.00)))
+                                .lineToLinearHeading(new Pose2d(23.58, 35.89, Math.toRadians(270.00)))
+                                .lineToSplineHeading(new Pose2d(49.06, 36.11, Math.toRadians(0.00)))
+                                .lineToLinearHeading(new Pose2d(49.27, 60.53, Math.toRadians(0.00)))
+                                .lineToLinearHeading(new Pose2d(55.43, 60.32, Math.toRadians(0.00)))
+                                .build()
 
                 );
 
--- MeepMeepPathing/src/main/java/com/example/meepmeeppathing/RedRightAuto.java
+++ MeepMeepPathing/src/main/java/com/example/meepmeeppathing/RedRightAuto.java
@@ -18,32 +18,27 @@ public class RedRightAuto {
                 // Option: Set theme. Default = ColorSchemeRedDark()
                 .followTrajectorySequence(drive ->
                                 // place pixel 1
-//                                drive.trajectorySequenceBuilder(new Pose2d(8.28, -63.29, Math.toRadians(90.00)))
-//                                        .splineTo(new Vector2d(17.42, -38.23), Math.toRadians(83.85))
-//                                        .splineTo(new Vector2d(0.64, -35.26), Math.toRadians(167.16))
-//                                        .splineTo(new Vector2d(24.64, -36.53), Math.toRadians(3.47))
-//                                        .splineTo(new Vector2d(43.12, -36.11), Math.toRadians(0.95))
-//                                        .splineTo(new Vector2d(50.76, -14.02), Math.toRadians(7.05))
-//                                        .splineTo(new Vector2d(60.96, -13.17), Math.toRadians(-2.49))
-//                                        .build()
+                               /* drive.trajectorySequenceBuilder(new Pose2d(9.13, -62.44, Math.toRadians(90.00)))
+                                        .lineToLinearHeading(new Pose2d(15.93, -35.89, Math.toRadians(90.00)))
+                                        .lineToLinearHeading(new Pose2d(0.85, -35.47, Math.toRadians(90.00)))
+                                        .lineToSplineHeading(new Pose2d(48.85, -36.53, Math.toRadians(0.00)))
+                                        .lineToLinearHeading(new Pose2d(48.64, -16.99, Math.toRadians(0.00)))
+                                        .build()
                         // place pixel 2
-//                        drive.trajectorySequenceBuilder(new Pose2d(9.56, -62.65, Math.toRadians(90.00)))
-//                                .splineTo(new Vector2d(16.35, -25.06), Math.toRadians(83.85))
-//                                .splineTo(new Vector2d(16.35, -9.13), Math.toRadians(-0.88))
-//                                .splineTo(new Vector2d(40.14, -20.39), Math.toRadians(-74.20))
-//                                .splineTo(new Vector2d(43.33, -31.65), Math.toRadians(-2.39))
-//                                .splineTo(new Vector2d(44.81, -32.92), Math.toRadians(-1.40))
-//                                .splineTo(new Vector2d(44.39, -11.89), Math.toRadians(3.47))
-//                                .splineTo(new Vector2d(61.17, -11.26), Math.toRadians(0.62))
-//                                .build()
+                                drive.trajectorySequenceBuilder(new Pose2d(9.13, -62.44, Math.toRadians(90.00)))
+                                        .lineToLinearHeading(new Pose2d(17.20, -24.85, Math.toRadians(90.00)))
+                                        .lineToSplineHeading(new Pose2d(48.85, -36.53, Math.toRadians(0.00)))
+                                        .lineToLinearHeading(new Pose2d(48.64, -16.99, Math.toRadians(0.00)))
+                                        .build() */
 
                         // place pixel 3
-                                drive.trajectorySequenceBuilder(new Pose2d(7.01, -61.59, Math.toRadians(90.00)))
-                                        .splineTo(new Vector2d(23.36, -35.26), Math.toRadians(58.16))
-                                        .splineTo(new Vector2d(44.39, -32.28), Math.toRadians(0.00))
-                                        .splineTo(new Vector2d(52.25, -18.48), Math.toRadians(38.37))
-                                        .splineTo(new Vector2d(62.23, -14.87), Math.toRadians(2.16))
-                                        .build()
+                        drive.trajectorySequenceBuilder(new Pose2d(9.13, -62.44, Math.toRadians(90.00)))
+                                .lineToLinearHeading(new Pose2d(23.58, -35.89, Math.toRadians(90.00)))
+                                .lineToSplineHeading(new Pose2d(48.85, -36.53, Math.toRadians(0.00)))
+                                .lineToLinearHeading(new Pose2d(48.64, -16.99, Math.toRadians(0.00)))
+                                .build()
+
+
 
 
                 );
